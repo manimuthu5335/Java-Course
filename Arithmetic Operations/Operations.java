d313e31f892dbdd597419cff0d27e33c45b2f153
manimuthu5335 <157777529+manimuthu5335@users.noreply.github.com>
2024-01-31 22:28:16 +0530

    Arithmetic Operations
    "Arithmetic Operation - is a function which can perform one of the following tasks: adding, subtracting, multiplying and dividing".

---
+++ Arithmetic Operations/Operations.java
@@ -0,0 +1,12 @@
+public class Operations{
+  public static void main(String[]args){
+   int a = 45,b = 76;
+   System.out.println("Addition is = "+(a+b));
+   System.out.println("Subtraction is = "+(a-b));
+   System.out.println("Multiplication is = "+(a*b));
+   System.out.println("Division is = "+(a/b));
+   System.out.println("Modulus is = "+(a%b));
+   
+   
+}
+}
