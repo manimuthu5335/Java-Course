6e7f056f3f9b021d265a1bfabc3aacb0b33afc35
manimuthu5335 <157777529+manimuthu5335@users.noreply.github.com>
2024-01-31 22:35:45 +0530

    Relational Operations
    "A relational operator is a programming language construct or operator that tests or defines some kind of relation between two entities. These include numerical equality (e.g., 5 = 5) and inequalities (e.g., 4 ≥ 3).".

---
+++ Relational Operations/relation.java
@@ -0,0 +1,11 @@
+public class relation{
+  public static void main(String[]args){
+    int x = 30,y = 40;
+   System.out.println("Greater than is = "+(x<y));
+   System.out.println("Less than is = "+(x>y));
+   System.out.println("Equal to is = "+(x=y));
+   System.out.println("Not Equal to is = "+(x!= y));
+   System.out.println("Greater than or Equal to is = "+(x<=y));
+   System.out.println("Less than or Equal to is = " +(x>=y));
+}
+}
