5e716e537333eda673660e8fd4f83d4bd461eb03
manimuthu5335 <157777529+manimuthu5335@users.noreply.github.com>
2024-01-31 23:11:33 +0530

    Increment Operator
    "The increment (++) operator (also known as increment unary operator) in Java is used to increase the value of a variable by 1. Since it is a type of a unary operator, it can be used with a single operand".

---
+++ Increment Operator/PreandPost.java
@@ -0,0 +1,29 @@
+public class PreandPost
+{
+  public static void main(String[]args){
+    int a=33;
+    int b=55;
+    a++;
+    System.out.println(a);
+    ++a;
+    System.out.println(a);
+
+    b++;
+    System.out.println(b);
+    ++b;
+    System.out.println(b);
+    
+    a--;
+    System.out.println(a);
+    --a;
+    System.out.println(a);
+   
+     b--;
+     System.out.println(b);
+     --b;
+     System.out.println(b);       
+   
+
+
+  }
+}
