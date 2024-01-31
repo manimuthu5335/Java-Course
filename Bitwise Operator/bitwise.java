1cc1098c18a6b90b0e9deda9fc37da72bb663623
manimuthu5335 <157777529+manimuthu5335@users.noreply.github.com>
2024-01-31 22:32:03 +0530

    Bitwise Operators
    "A bitwise operator in Java is a symbol/notation that performs a specified operation on standalone bits, taken one at a time. It is used to manipulate individual bits of a binary number and can be used with a variety of integer types – char, int, long, short, byte".

---
+++ Bitwise Operator/bitwise.java
@@ -0,0 +1,11 @@
+public class bitwise{
+  public static void main(String[]args){
+   int a =333;
+   int b =555;
+   System.out.println("and is "+(a&b));
+   System.out.println("or is "+(a|b));
+   System.out.println("xor is "+(a^b));
+
+
+}
+}
