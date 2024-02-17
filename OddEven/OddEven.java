fa4c27eab194f97c2e684e72f6c38e505ca1ddb1
Manimuthu <157777529+manimuthu5335@users.noreply.github.com>
2024-02-17 09:50:06 +0530

    Add files via upload

---
+++ OddEven/OddEven.java
@@ -0,0 +1,24 @@
+public  class OddEven{
+ public static void main(String[]args)
+ { 
+    //  To find OddNumber
+    System.out.println("Odd Numbers Between 1 to 20 ");
+     for(int i=1;i<=20;i++){
+       if(i%2==1)
+       System.out.println(i);
+ }
+     System.out.println("-------------------------------");
+    // To find EvenNumber
+     System.out.println("Even Numbers Between 1 to 20 ");
+    for(int j=1;j<=20;j++){
+ if(j%2==0)
+     System.out.println(j);	
+       
+}
+          
+
+           
+
+ 
+}
+}
