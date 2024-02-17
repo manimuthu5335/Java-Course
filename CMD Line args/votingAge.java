7dff7b66e53d42351983ed3a25efc71fdedbe040
Manimuthu <157777529+manimuthu5335@users.noreply.github.com>
2024-02-17 09:05:38 +0530

    Update votingAge.java

--- CMD Line args/votingAge.java
+++ CMD Line args/votingAge.java
@@ -1,13 +1,15 @@
 public class votingAge{
-public static void main(String[]args){
-
-int age = Integer.parseInt(args[0]);
+public static void main(String[]  args){
+ 
+String Name = args[0];
+System.out.println(Name);
+int age = Integer.parseInt(args[1]);
 System.out.println(age);
 if(age>=18){
-System.out.println(age +" is Eligible");
+System.out.println(Name +" is Eligible");
 }
 else{
-System.out.println(age +" is Not Eligible");
+System.out.println(Name +" is Not Eligible to vote");
 }
 
 }
